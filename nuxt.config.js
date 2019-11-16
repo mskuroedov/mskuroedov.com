6821bd2fac30620bac2968b9ab5b68fdb6584138
Maxim <mskuroedov@gmail.com>
2019-11-16 17:44:05 +0300

    dist added

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ export default {
   /*
   ** Nuxt.js dev-modules
   */
-  devModules: [],
+  buildModules: [],
   /*
   ** Nuxt.js modules
   */
@@ -64,12 +64,24 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    postcss: [
-      require('autoprefixer'),
-      require('postcss-nested')(),
-      require('postcss-responsive-type')(),
-      require('postcss-hexrgba')()
-    ],
+
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        'postcss-url': false,
+        'postcss-nested': {},
+        'postcss-responsive-type': {},
+        'postcss-hexrgba': {}
+      },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {
+          grid: true
+        }
+      }
+    },
     extend(config, ctx) {
     }
   }
